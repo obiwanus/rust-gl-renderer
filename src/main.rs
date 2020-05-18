c2842c8cef56567439d55556a65c87dab336f151
Ivan Ivanov <ivan@ivanovs.info>
2020-05-18 22:03:34 +0200

    Trying to look back at vscode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+// ==================================== Imports ==================================================
+use sdl2;
+
+// ==================================== Types ====================================================
+
+// ==================================== Functions ================================================
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(error) = run() {
+        eprintln!("{}", error);
+        std::process::exit(1);
+    }
+}
+
+fn run() -> Result<(), Box<dyn std::error::Error>> {
+    let sdl = sdl2::init()?;
+    let video_subsystem = sdl.video()?;
+
+
+
+    Ok(())
 }
